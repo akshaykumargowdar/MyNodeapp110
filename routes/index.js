b59968e7ceeb22a38bc46d4ee240dd6b042ff98f
akshaykumargowdar <akshaykumargowdar@gmail.com>
2017-04-21 12:59:50 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var fs = require('file-system');
+var router = express.Router();
+
+var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+
+var text_to_speech = new TextToSpeechV1({
+    username: '69bfd0da-dd82-416c-8043-901a66dda6b0',
+    password: 'yT2ZMmqPKrDv',
+    headers: {
+    'X-Watson-Learning-Opt-Out': 'true'
+    }
+});
+
+var speech_to_text = new SpeechToTextV1({
+    username : "87f6b0a7-4d1c-4c27-ae4b-177463cfd33f",
+    password : "iDzdOBlQ61v2" ,
+	headers: {
+    'X-Watson-Learning-Opt-Out': 'true'
+  }
+});
+
+router.get('/texttospeech', function(req, res, next) {
+	var params = {
+  text: 'Welocme to node js',
+  voice: 'en-US_AllisonVoice',
+  accept: 'audio/wav'
+};
+// Pipe the synthesized text to a file.
+text_to_speech.synthesize(params).on('error', function(error) {
+  console.log('Error:', error);
+}).pipe(fs.createWriteStream('./public/audio/audio1.wav'));
+	
+});
+  
+ 
+router.get('/speechtotext', function(req, res, next) {
+	
+    var params = {
+    audio: fs.createReadStream('./public/audio/audio1.wav'),
+    content_type: 'audio/wav',
+    timestamps: true,
+    word_alternatives_threshold: 0.9,
+    continuous: true
+  };
+// Pipe the synthesized text to a file.
+ speech_to_text.recognize(params, function(error, transcript) {
+    if (error)
+      console.log('Error:', error);
+    else
+      res.send(transcript.results[0].alternatives[0].transcript);
+      
+  });
+	
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
