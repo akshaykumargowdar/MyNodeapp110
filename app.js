61124220e0b402fdb23617e7a099e8a3d1391c16
Mohammed Abrar <mohammed.abrar95@gmail.com>
2017-04-21 13:11:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -16,14 +8,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -56,12 +43,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
 module.exports = app;
