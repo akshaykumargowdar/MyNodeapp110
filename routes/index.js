5ec8a0efc55437bfc158ffbfd1e39156ca3d84db
Mohammed Abrar <mohammed.abrar95@gmail.com>
2017-04-24 17:27:38 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -61,7 +61,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('./conversationapp', function(req,res,next) { 
+router.get('/conversationapp', function(req,res,next) { 
 	res.render('conversation');
 });
 module.exports = router;
